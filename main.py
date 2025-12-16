da5ec16d6984397edd2955eae47b9ba9a25f154c
Kishan Garhwal <surendrakuma123@gmail.com>
2025-12-16 14:51:59 +0530

    update MatchResult Class

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
+from typing import List
 from sentence_transformers import SentenceTransformer
 from sklearn.metrics.pairwise import cosine_similarity
 
@@ -9,37 +10,53 @@ app = FastAPI(
     version="1.0"
 )
 
-# Load model once (important)
+ 
 model = SentenceTransformer("all-MiniLM-L6-v2")
+
+ 
+
+class FoundItem(BaseModel):
+    postId: str
+    description: str
+
 
 class MatchRequest(BaseModel):
     lost_text: str
-    found_items: list[str]
+    found_items: List[FoundItem]
+ 
 
 class MatchResult(BaseModel):
+    postId: str
     item: str
     match_percent: float
-    
+
+
 @app.get("/")
 def root():
     return {"status": "DropZone AI backend is running"}
 
-@app.post("/match", response_model=list[MatchResult])
-def match_items(data: MatchRequest):
-    lost_embedding = model.encode([data.lost_text])[0]
-    found_embeddings = model.encode(data.found_items)
 
+@app.post("/match", response_model=List[MatchResult])
+def match_items(data: MatchRequest):
+ 
+    lost_embedding = model.encode([data.lost_text])[0]
+ 
+    found_descriptions = [item.description for item in data.found_items]
+    found_embeddings = model.encode(found_descriptions)
+ 
     scores = cosine_similarity(
         [lost_embedding],
         found_embeddings
     )[0]
-
+ 
     results = []
-    for i, score in enumerate(scores):
+    for idx, score in enumerate(scores):
         results.append({
-            "item": data.found_items[i],
+            "postId": data.found_items[idx].postId,
+            "item": data.found_items[idx].description,
             "match_percent": round(float(score * 100), 2)
         })
-
+ 
     results.sort(key=lambda x: x["match_percent"], reverse=True)
+
     return results[:5]
